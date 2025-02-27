a5656e1fcd81c47d28ed6af458655140b3d27325
unknown <luiscesar.munoz.cervantes.upiit@gmail.com>
2025-02-27 13:36:09 -0600

    add data service boletin rt32

--- src/components/FinalSummary.js
+++ src/components/FinalSummary.js
@@ -17,7 +17,9 @@ const FinalSummary = ({ selections }) => {
       <hr/>
        {selections.finalRepair === 'M09-AJUSTE' && <div>
           <h3>Proceso correcto del cierre de boletin RT5300 , RT6300</h3>   
-           <p>Una vez agregados los codigos correctos generaremos la saw para el extra de pago en mano<br/>de esta forma se pagara 200 mas 160 de la extra saw, es necesario subir toda la informacion<br/>solicitada, El RT adjunto, evidencia de que se utilizo la camara endoscopica y la saw indicando<br/>"Pago de extra mano, aplicacion de service boletin"  </p>    
+           <p>Aplicar los boletines de servicio para estos modelos,<strong>1 Boletin: Actualizacion de sowftware.</strong> Los cuales se encuentran en GSPN, <br /><strong>2 Boletin: Inspeccion de ductos, correcto sellado de puerta, uso de arandela y camara endoscopica</strong><br />La aplicacion del primer boletin no inhibe la aplicacion del segundo boletin.</p>
+           <p>Una vez agregados los codigos correctos generaremos la saw para el extra de pago en mano<br/>de esta forma se pagara 200 mas 160 de la extra saw, es necesario subir toda la informacion<br/>solicitada, El RT adjunto, evidencia de que se utilizo la camara endoscopica y la saw indicando<br/>"Pago de extra mano, aplicacion de service boletin"  </p> 
+    
         </div>}
         {selections.finalRepair === 'M09-AJUSTE.' && <div>
           <h3>Cierre en caso de cambio de espreas</h3>
